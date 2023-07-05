4148a86f083201d3236e83c53f1b631cd45ed4de
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-07-05 18:30:27 +0300

    Updated NASA_api_sync.py, weather.py. Created NASA_api_async.py.

--- NASA_api_sync.py
+++ NASA_api_sync.py
@@ -9,7 +9,7 @@ load_dotenv()
 API_KEY = os.getenv("NASA_API_KEY")
 random_dates = []
 
-for _ in range(50):
+for _ in range(20):
     random_dates.append(f"{randint(2000, 2022)}-{randint(1, 12)}-{randint(1, 28)}")
 
 
@@ -17,16 +17,16 @@ def get_image_data(api_key, dates):
     for i, date in enumerate(dates):
         response = requests.get(
             f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={date}")
-        data = response.json()
+        json_data = response.json()
         print(i, {
-            'title': data["title"],
-            'url': data["url"],
-            'media_type': data["media_type"]
+            'title': json_data["title"],
+            'url': json_data["url"],
+            'media_type': json_data["media_type"]
         })
 
 
 start_time = time.time()
-print(get_image_data(API_KEY, random_dates))
+get_image_data(API_KEY, random_dates)
 end_time = time.time() - start_time
 
 print(end_time, "seconds.")
--- weather.py
+++ weather.py
@@ -13,13 +13,13 @@ coordinates = [(47.2313500, 39.7232800), (55.7522, 37.6156), (43.5992, 39.7257),
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        i = 1
+        number = 1
         for lat, lon in coordinates:
             async with session.get(
                     f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}") as response:
-                json = await response.json()
-                print(i, {"status": response.status, "city": json["name"], "tempature": round(int(json["main"]["temp"]) - 273.15, 2)})
-                i += 1
+                json_data = await response.json()
+                print(number, {"status": response.status, "city": json_data["name"], "tempature": round(int(json_data["main"]["temp"]) - 273.15, 2)})
+                number += 1
 
 
 if __name__ == "__main__":
